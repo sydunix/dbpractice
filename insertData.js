377f5adee437962b8d4da37319d7bcc956ad0623
sjekpo <sydney.j.ekpo@gmail.com>
2021-04-13 11:38:37 +0100

    added April Dates

--- insertData.js
+++ insertData.js
@@ -20,14 +20,14 @@ con.connect(function(err) {
 
   let sql = `INSERT INTO mentees (id, name, age, department, role, status, createdAt)
   VALUES 
-  ('1', 'Sarah', '5', 'Accounting', 'PM' , 'Activated', '2020-01-01'),
-  ('2', 'Tim', '10', 'Engineering', 'QA' , 'Pending', '2021-02-01'),
-  ('3', 'Joe', '17', 'Management', 'PM' , 'Activated', '2021-02-01'),
-  ('4', 'Tolu', '25', 'Management', 'Dev' , 'Pending', '2021-02-01'),
-  ('5', 'Rob', '5', 'Engineering', 'QA' , 'Activated', '2021-03-01'),
-  ('6', 'Ade', '10', 'Management', 'QA' , 'Pending', '2021-04-01'),
-  ('7', 'Tom', '17', 'Security', 'QA' , 'Activated', '2021-05-01'),
-  ('8', 'Jide', '26', 'Accounting', 'Dev' , 'Activated', '2021-06-01') 
+  ('1', 'Sarah', '5', 'Accounting', 'PM' , 'Activated', '2021-04-01'),
+  ('2', 'Tim', '10', 'Engineering', 'QA' , 'Pending', '2021-04-02'),
+  ('3', 'Joe', '17', 'Management', 'PM' , 'Activated', '2021-04-03'),
+  ('4', 'Tolu', '25', 'Management', 'Dev' , 'Pending', '2021-04-03'),
+  ('5', 'Rob', '5', 'Engineering', 'QA' , 'Activated', '2021-04-04'),
+  ('6', 'Ade', '10', 'Management', 'QA' , 'Pending', '2021-04-05'),
+  ('7', 'Tom', '17', 'Security', 'QA' , 'Activated', '2021-04-05'),
+  ('8', 'Jide', '26', 'Accounting', 'Dev' , 'Activated', '2021-04-07')
   `;
   
   let tableCreateMsg = (err, result) => {if (err) throw err; console.log("Values insterted in Table"); }
